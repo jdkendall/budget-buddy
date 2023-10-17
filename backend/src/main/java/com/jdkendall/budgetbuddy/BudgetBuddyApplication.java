d53e4d12a1c6f3f5d2cef7b2fedc640fa033a118
Justin D Kendall <justin.d.kendall@gmail.com>
2023-10-17 13:18:11 -0500

    Initial commit

---
+++ backend/src/main/java/com/jdkendall/budgetbuddy/BudgetBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.jdkendall.budgetbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetBuddyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jdkendall/budgetbuddy/BudgetBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdkendall.budgetbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
