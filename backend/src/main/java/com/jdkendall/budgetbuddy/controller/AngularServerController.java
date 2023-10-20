b1b070b5de8a3f4868b3731ed1a6a6ef981a2e26
Justin D Kendall <justin.d.kendall@gmail.com>
2023-10-20 00:29:41 -0500

    Fix wildcard pattern on Angular server

--- backend/src/main/java/com/jdkendall/budgetbuddy/controller/AngularServerController.java
+++ backend/src/main/java/com/jdkendall/budgetbuddy/controller/AngularServerController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AngularServerController {
-    @RequestMapping(value = "/**/*", produces = MediaType.TEXT_HTML_VALUE)
+    @RequestMapping(value = "/**", produces = MediaType.TEXT_HTML_VALUE)
     public String forwardToAngularRoute() {
         return "forward:/index.html";
     }
