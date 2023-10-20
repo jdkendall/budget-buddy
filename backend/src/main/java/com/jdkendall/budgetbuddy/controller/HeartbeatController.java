7ff3d0e62c65e87a11b58a6531215bee53319649
Justin D Kendall <justin.d.kendall@gmail.com>
2023-10-19 22:38:05 -0500

    Move heartbeat route outside api path and fix Angular serving controller

--- backend/src/main/java/com/jdkendall/budgetbuddy/controller/HeartbeatController.java
+++ backend/src/main/java/com/jdkendall/budgetbuddy/controller/HeartbeatController.java
@@ -4,7 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/api/heartbeat")
+@RequestMapping("/heartbeat")
 public class HeartbeatController {
     @GetMapping
     public ResponseEntity<Void> heartbeat() {
